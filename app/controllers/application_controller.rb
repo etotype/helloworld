11f04f94a08d76c14cd46f9987692196639bad4f
etotype <etotype@gmail.com>
2020-04-07 00:54:25 -0500

    First one!

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::Base
+
+    def hello
+      render html: ("<p>Hello worlddd &#x1F525;!!!<p>").html_safe
+    end
+
+end
---
+++ setup.md
@@ -0,0 +1,3 @@
+# How to setup Rails on Windows 10 for remote development
+## Purpose
+This simple repo
